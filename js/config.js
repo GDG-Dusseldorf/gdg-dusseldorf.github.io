2a8db9aee0998801dc5ad831dc5c04be289a2c6a
Randy Nel Gupta <gupta@scaratec.com>
2014-09-10 19:16:29 +0200

    Updated data for GDG Düsseldorf except for picasa.

--- js/config.js
+++ js/config.js
@@ -1,17 +1,17 @@
 boomerang.factory('Config', function () {
     return {
         //modify these
-        'name'          : 'GDG Space Coast',
-        'id'            : '103959793061819610212',
-        'google_api'    : 'AIzaSyA9ALjr2iWvhf3Rsz9-bH0cEcDcrdkpuAg',
+        'name'          : 'GDG Düsseldorf',
+        'id'            : '107373371386267684213',
+        'google_api'    : 'AIzaSyBQ3n5zeiEV7aWIurOcPCyFoYJpPErtx9Y',
         'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
-        'domain'        : 'http://www.gdgspacecoast.org',
+        'domain'        : 'http://www.gdg-dus.de',
         'cover' : {
-            title: 'Google I/O 2014',
-            subtitle: 'Google\'s yearly developer event is being held June 25-26th in San Francisco, CA.',
+            title: 'DevFest 2014',
+            subtitle: 'The DevFest 2014 is taking place on October 23rd in Düsseldorf!',
             button: {
                 text: 'Find out more',
-                url: 'https://www.google.com/events/io'
+                url: 'http://gdg-dusseldorf.github.io/DevFest2014/'
             }
         }
     };
