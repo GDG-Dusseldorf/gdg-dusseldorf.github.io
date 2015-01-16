8b84b81726bf407fe48ab7bc6bc88fdd4776ea5e
Jens Hauke <jens@4k2.de>
2015-01-16 09:11:33 +0100

    Meetup "Android Auto" and "Scripting"

--- js/config.js
+++ js/config.js
@@ -7,11 +7,11 @@ boomerang.factory('Config', function () {
         'pwa_id'        : '5915725140705884785', //picasa web album id, must belong to google+ id above
         'domain'        : 'http://www.gdg-dus.de',
         'cover' : {
-            title: 'DevFest 2014',
-            subtitle: 'The DevFest 2014 took place on October 26th in Düsseldorf. Thank you all for attending and making it a great success!',
+            title: 'Meetup: "Android Auto" and "Scripting"',
+            subtitle: 'Our first meetup in 2015 took place on January 30th in Düsseldorf @ Garage Bilk.',
             button: {
                 text: 'Find out more',
-                url: 'http://www.gdg-dus.de/DevFest2014/'
+                url: 'https://plus.google.com/u/0/events/c8mh12mukapsn9i5u1tp7v8qlr8'
             }
         }
     };
